222c12f3764cbec579a70d2741ffc38f518201cd
HuynhManhHung441 <huynhmanhhung441@gmail.com>
2023-08-12 16:57:06 +0700

    generate brick, brick drop down, handleLanded, handdleCompleteRows

--- scripts/index.js
+++ scripts/index.js
@@ -213,20 +213,33 @@ class Board {
     drawBoard() {
         for (let row = 0; row < this.grid.length; row++) {
             for (let col = 0; col < this.grid[row].length; col++) {
-                this.drawCell(col, row, WHITE_COLOR_ID);
+                this.drawCell(col, row, this.grid[row][col]);
             }
         }
+    }
+
+    handleCompleteRows() {
+      const latestGrid = board.grid.filter((row) => {
+        return row.some(col => col === WHITE_COLOR_ID);
+      });
+
+      const newScore = ROWS - latestGrid.length;
+      const newRows = Array.from({length: newScore}, ()=>Array(COLS).fill(WHITE_COLOR_ID));
+
+      board.grid = [...newRows, ...latestGrid];
+
+      console.log({latestGrid});
     }
 }
 
 
 class Brick {
-    constructor(id, now_colPos, now_rowPos) {
+    constructor(id) {
         this.id = id;
         this.layout = BRICK_LAYOUT[id];
         this.activeIndex = 0;
-        this.colPos = now_colPos;
-        this.rowPos = now_rowPos;
+        this.colPos = 3;
+        this.rowPos = 0;
     }
 
     draw() {
@@ -270,7 +283,11 @@ class Brick {
         this.clear();
         this.rowPos++;
         this.draw();
+        return;
       }
+      this.handleLanded();
+      generateNewBrick();
+
     }
 
     rotate () {
@@ -285,23 +302,43 @@ class Brick {
       for (let row = 0; row < nextLayout.length; row++) {
         for (let col = 0; col < nextLayout[row].length; col++) {
             if (nextLayout[row][col] !== WHITE_COLOR_ID) {
-              if ((col + nextCol < 0)||(col + nextCol >= COLS)||(row + nextRow >=ROWS)) return true;
+              if ((col + nextCol < 0)||
+                  (col + nextCol >= COLS)||
+                  (row + nextRow >=ROWS)||
+                  board.grid[row + nextRow][col + nextCol] !== WHITE_COLOR_ID
+                  )
+                return true;
             }
         }
+      }
     }
+    handleLanded () {
+      for (let row = 0; row < this.layout[this.activeIndex].length; row++) {
+        for (let col = 0; col < this.layout[this.activeIndex][row].length; col++) {
+            if (this.layout[this.activeIndex][row][col] !== WHITE_COLOR_ID) {
+              board.grid[row + this.rowPos][col + this.colPos] = this.id;
+            }
+        }
+      }
+      board.handleCompleteRows();
+      board.drawBoard();
     }
 }
 
 
+function generateNewBrick() {
+  brick = new Brick(Math.floor(Math.random() * 10) % BRICK_LAYOUT.length);
+}
+
 board = new Board(ctx);
-// board.drawCell(1,1,1);
 board.drawBoard();
-brick = new Brick(0, 3, 3);
+// brick = new Brick(0);
+generateNewBrick();
 brick.draw();
-brick.moveLeft();
-brick.moveRight();
-brick.moveDown();
-brick.rotate();
+
+setInterval(() => {
+  brick.moveDown();
+}, 1000);
 
 document.addEventListener('keydown', (e) => {
   console.log({e});
